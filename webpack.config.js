6ac199fa98390accb796967949b8c0e5d12ca4a9
Andrey Sysoev <andrey@sysoev.ru>
2020-07-19 23:12:22 +0300

    lodash new version 19

--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,7 @@ module.exports = {
                         importLoaders: 2
                     }
                 },
-                    'postcss-loader'
+                'postcss-loader'
             ]
         },
         ]
